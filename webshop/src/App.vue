5ba77228dd5275eed99319fa73cfff395c3d5738
I-like-PhysiX <32261812+I-like-PhysiX@users.noreply.github.com>
2018-08-12 11:13:59 +0200

    Update App.vue

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -1,83 +1,83 @@
 <template>
-  <div id ="app">
+  <div id="app">
     <b-navbar toggleable="md" variant="dark" sticky>
-      <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
-        <div id="myContainer">
-           <div class="my-3">
-             <b-button-group>
-               <b-btn
-                       class="exPopoverReactive1"
-                       size="md"
-                       variant="dark"
-                       style="text-align: center"
-                       v-on:click="init(), search='', sortType='', selected=''">
-                       Webshop
-               </b-btn>
+     <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+       <div id="myContainer">
+          <div class="my-3">
+            <b-button-group>
+              <b-btn
+                      class="exPopoverReactive1"
+                      size="md"
+                      variant="dark"
+                      style="text-align: center"
+                      v-on:click="init(), search='', sortType='', selected=''">
+                      Webshop
+              </b-btn>
+            <b-btn
+                   class="exPopoverReactive1"
+                   v-on:click="search='', csaktermekek=false, csakkosar=true, basketrouter()"
+                   v-if="csaktermekek"
+                   size="md"
+                   variant="dark"
+                   style="text-align: center">
+                   {{this.rendeleshossz}} tétel
+             </b-btn>
              <b-btn
                     class="exPopoverReactive1"
-                    v-on:click="search='', csaktermekek=false, csakkosar=true, basketrouter()"
-                    v-if="csaktermekek"
+                    v-on:click="onCancel"
+                    v-if="csakkosar"
                     size="md"
                     variant="dark"
                     style="text-align: center">
-                    {{this.rendeleshossz}} tétel
+                    Termékek
               </b-btn>
-              <b-btn
-                     class="exPopoverReactive1"
-                     v-on:click="onCancel"
-                     v-if="csakkosar"
-                     size="md"
-                     variant="dark"
-                     style="text-align: center">
-                     Termékek
-               </b-btn>
-              </b-button-group>
-           </div>
-    </div>
-    <b-collapse is-nav id="nav_collapse">
-      <b-navbar-nav class="ml-auto">
-        <ul class="navbar-nav mr-auto" style="margin-top: 15px;">
-          <li class="nav-item active">
-            <select id="selecttype" v-model="selected" v-on:change="szur()" class="form-control input-sm">
-              <option value="" disabled>Termékfajták</option>
-              <option v-for="(item, index) in this.species" :key="item.text" :value="item.value" :disabled="item.isdisabled">{{item.text}}</option>
-            </select>
-          </li>
-          <li class="nav-item active">
-            <select name="sortBy" id="sortBy" v-on:change="szur()" v-model="sortType" class="form-control input-sm">
-              <option v-for="(item, index) in sortOptions" :key="item.text" :value="item.value" :disabled="item.isdisabled">{{item.text}}</option>
-            </select>
-          </li>
-          <li class="nav-item active">
-            <b-form-group
-                          label-for="search"
-                          :state="searchstate">
-              <b-form-input
-                            class="form-control"
-                            size="md"
-                            placeholder="Keresés"
-                            type="text"
-                            id="search"
-                            v-model="search"/>
-            </b-form-group>
-          </li>
-          <li class="nav-item active">
-            <b-btn
-                          class="exPopoverReactive1"
-                          v-on:click="szur2(), search=''"
-                          size="md"
-                          variant="dark"
-                          style="text-align: center"
-                          :disabled="this.search === '' ? true : false">
-                          Keresés
-            </b-btn>
-          </li>
-        </ul>
-       </b-navbar-nav>
-     </b-collapse>
-   </b-navbar>
+             </b-button-group>
+          </div>
+   </div>
+   <b-collapse is-nav id="nav_collapse">
+     <b-navbar-nav class="ml-auto">
+       <ul class="navbar-nav mr-auto" style="margin-top: 15px;">
+         <li class="nav-item active">
+           <select id="selecttype" v-model="selected" v-on:change="szur()" class="form-control input-sm">
+             <option value="" disabled>Termékfajták</option>
+             <option v-for="(item, index) in this.species" :key="item.text" :value="item.value" :disabled="item.isdisabled">{{item.text}}</option>
+           </select>
+         </li>
+         <li class="nav-item active">
+           <select name="sortBy" id="sortBy" v-on:change="szur()" v-model="sortType" class="form-control input-sm">
+             <option v-for="(item, index) in sortOptions" :key="item.text" :value="item.value" :disabled="item.isdisabled">{{item.text}}</option>
+           </select>
+         </li>
+         <li class="nav-item active">
+           <b-form-group
+                         label-for="search"
+                         :state="searchstate">
+             <b-form-input
+                           class="form-control"
+                           size="md"
+                           placeholder="Keresés"
+                           type="text"
+                           id="search"
+                           v-model="search"/>
+           </b-form-group>
+         </li>
+         <li class="nav-item active">
+           <b-btn
+                         class="exPopoverReactive1"
+                         v-on:click="szur2(), search=''"
+                         size="md"
+                         variant="dark"
+                         style="text-align: center"
+                         :disabled="this.search === '' ? true : false">
+                         Keresés
+           </b-btn>
+         </li>
+       </ul>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 
-   <router-view
+    <router-view
                 :paginatedDatacopy="paginatedData"
                 :itemsPerRowcopy="itemsPerRow"
                 :szurttombhosszcopy="szurttombhossz"
@@ -94,18 +94,19 @@
                 v-on:datarouterEvent="datarouter"
                 v-on:initEvent="init"
                 v-on:basketrouterEvent="basketrouter">
-   </router-view>
+    </router-view>
 
-   <b-navbar toggleable="md" variant="dark" class="bottom">
-    <div class="footer" style="color:white;  margin: 0px auto;">
-      © 2018. Minden jog fenntartva
-    </div>
-   </b-navbar>
+    <b-navbar toggleable="md" variant="dark" class="bottom">
+      <div class="footer" style="color:white;  margin: 0px auto;">
+        © 2018. Minden jog fenntartva
+      </div>
+    </b-navbar>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
 export default {
   name: "app",
   data() {
@@ -154,8 +155,7 @@ export default {
       elem.alap = Math.round((elem.alap + step) * 10) / 10;
       this.rendeles.push(elem);
       this.rendeles=Array.from(new Set(this.rendeles));
-      var rendeleshossz=this.rendeles.length;
-      this.rendeleshossz=rendeleshossz;
+      this.rendeleshossz=this.rendeles.length;
     },
     removefromcart(elem, step){
       elem.alap = Math.round((elem.alap - step) * 10) / 10;
@@ -166,15 +166,12 @@ export default {
     del(elem){
       elem.alap=0;
       this.rendeles.splice(this.rendeles.indexOf(elem), 1);
-      var rendeleshossz=this.rendeles.length;
-      this.rendeleshossz=rendeleshossz;
+      this.rendeleshossz=this.rendeles.length;
     },
     init(){
-      var szurttomb = this.tomb.filter(v => v.info=="Akció!");
-      this.szurttomb=szurttomb;
+      this.szurttomb = this.tomb.filter(v => v.info=="Akció!");
       this.itemsPerRow=this.szurttomb.length;
-      var szurttombhossz=this.szurttomb.length;
-      this.szurttombhossz=szurttombhossz;
+      this.szurttombhossz=this.szurttomb.length;
       this.onCancel();
       this.initrouter();
     },
@@ -182,14 +179,12 @@ export default {
       let mySet = new Set();
       this.tomb.forEach(v => mySet.add(v.type));
       let myArray = Array.from(mySet);
-      var species=[];
-      myArray.forEach(v => species.push({
+      myArray.forEach(v => this.species.push({
                                           text: v,
                                           value: v,
                                           isdisabled: false
                                        })
       );
-      this.species=species;
     },
     onCancel(){
       this.initrouter();
@@ -241,8 +236,7 @@ export default {
             };
         this.szurttomb=szurttomb;
         this.itemsPerRow=this.szurttomb.length;
-        var szurttombhossz=this.szurttomb.length;
-        this.szurttombhossz=szurttombhossz;
+        this.szurttombhossz=this.szurttomb.length;
         this.categoryrouter();
       },
       szur2(){
@@ -251,16 +245,13 @@ export default {
         this.csakkosar=false;
         this.csaktermekek=true;
         if (this.search) {
-          var szurttomb =this.tomb.filter(v => RegExp(this.search,'i').test(v.termek)).slice(0,9);
-          this.szurttomb=szurttomb;
+          this.szurttomb =this.tomb.filter(v => RegExp(this.search,'i').test(v.termek)).slice(0,9);
           this.itemsPerRow=this.szurttomb.length;
-          var szurttombhossz=this.szurttomb.length;
-          this.szurttombhossz=szurttombhossz;
+          this.szurttombhossz=this.szurttomb.length;
           this.searchrouter();
         }
       }
   },
-
   mounted() {
     axios.get('http://localhost:3000')
       .then((res) =>{
@@ -295,61 +286,34 @@ export default {
 </script>
 
 <style scoped>
- #App{
-  font-family: courier;
-  box-sizing: border-box;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-    -webkit-box-orient: vertical;
-    -webkit-box-direction: normal;
-    -webkit-flex-direction: column;
-    -ms-flex-direction: column;
-    flex-direction: column;
-    margin: 0 auto;
-    min-height: 100vh;
- }
- .main{
-  width:100%;
-  overflow:hidden;
+#app{
+ font-family: courier;
+ box-sizing: border-box;
+   display: -webkit-box;
+   display: -webkit-flex;
+   display: -ms-flexbox;
+   display: flex;
+   -webkit-box-orient: vertical;
+   -webkit-box-direction: normal;
+   -webkit-flex-direction: column;
+   -ms-flex-direction: column;
+   flex-direction: column;
+   margin: 0 auto;
+   min-height: 100vh;
+}
+.main{
+ width:100%;
+ overflow:hidden;
 }
 .bottom,.text-mid {
-  padding:20px 10px;
-  height:100%;
+ padding:20px 10px;
+ height:100%;
 }
 .text-mid {
-  margin-top: auto;
-  background:white;
+ margin-top: auto;
+ background:white;
 }
- .form-control {
-    display: inline-block;
-}
-#card:hover{
-  border-width:1px;
-  border-style:ridge;
-  border-color:#4285F4;
-}
-.exPopoverReactive1:hover{
-  border-width:1px;
-  border-style:ridge;
-  border-color:#FFFFFF;
-}
-.image {
-    position: relative;
-}
-.top-right {
-    position: absolute;
-    top: 8px;
-    right: 16px;
-}
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-th, td {
-    padding: 8px;
-    text-align: left;
-    border-bottom: 1px solid #ddd;
+.form-control {
+   display: inline-block;
 }
 </style>
